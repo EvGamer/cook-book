9b7cdc5a9a4d1284e4540a41e515d880b8250f01
Eugene <e.kuzmenko@dunice.net>
2018-12-03 09:58:38 +0300

    Initialization

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+/* eslint react/jsx-filename-extension:"off" */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'stretch',
+  },
+  header: {
+    height: 40,
+    backgroundColor: '#20436E',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  button: {
+    width: 25,
+    height: 25,
+    fontSize: 20,
+    backgroundColor: '#b7c6d6',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          Items
+        </Text>
+      </View>
+    </View>
+  );
+}
---
+++ screens/item-list/ItemList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {View} from 'react-native';
+import style from './style';
+
+export default function ItemList({navigation}) {
+return (
+    <View style={style.screen}>
+    </View>
+  );
+}
+
+
+ItemList.propTypes = {
+  navigation: PropTypes.object,
+};
